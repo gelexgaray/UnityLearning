a4825e58a6cbd3e7ed3989bac48d3b323c02459d
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-01-05 09:58:22 +0100

    Make the farmer jump

---
+++ 05_Jumping_Farmer/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpImpulse = 10;
+    private bool isOnGround = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            rb.AddForce(Vector3.up * jumpImpulse, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
