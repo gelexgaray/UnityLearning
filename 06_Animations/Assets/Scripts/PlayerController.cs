bbbc5ccb7e6d60fe6e6b4f9b9506b173a8b64adc
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-01-17 10:34:11 +0100

    Basic animation sample

---
+++ 06_Animations/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Constants
+    private const string FLAP = "Flap";
+    #endregion
+
+    private Animator Animator { get => _animator; }
+    private Animator _animator;
+
+    public bool Flap
+    {
+        get => _isFlapping;
+        set
+        {
+            _isFlapping = value;
+            Animator.SetBool(FLAP, value);
+        }
+    }
+    private bool _isFlapping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flap = Input.GetKey(KeyCode.Space);
+    }
+}
