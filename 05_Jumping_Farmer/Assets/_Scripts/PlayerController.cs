15825a6eaed793cde3716a3209aeb8e29e33843e
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-01-31 09:28:01 +0100

    Add particle systems to farmer running game

--- 05_Jumping_Farmer/Assets/_Scripts/PlayerController.cs
+++ 05_Jumping_Farmer/Assets/_Scripts/PlayerController.cs
@@ -11,10 +11,26 @@ public class PlayerController : MonoBehaviour
     private Animator _animator;
     private Rigidbody _rigidBody;
 
+    [SerializeField, Range(0, 2)]
+    private float trailThreshold = 1.5f;
+
     public float jumpImpulse = 8;
     private bool isOnGround = true;
 
+    public ParticleSystem explossion;
+    public ParticleSystem trail;
+
     public bool GameOver { get; private set; }
+
+    private float GameSpeed {
+        get => _gameSpeed;
+        set { 
+            _gameSpeed = value;
+            Time.timeScale =_gameSpeed;
+        } 
+    }
+    [SerializeField]
+    private float _gameSpeed;
 
 
     // Start is called before the first frame update
@@ -27,14 +43,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GameOver) return;
+        ShowTrailIfRunningHighSpeed();
+        if (GameOver) return; 
 
+        // The following code runs only on active game play
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             _rigidBody.AddForce(Vector3.up * jumpImpulse, ForceMode.Impulse);
             isOnGround = false;
             _animator.SetTrigger(JUMP_TRIGGER);
         }
+
+        // Increase game speed over time
+        GameSpeed = 1 + Time.time / 50;
+    }
+
+    private void ShowTrailIfRunningHighSpeed() 
+    {
+        if (null == trail) return;
+        bool enableTrail = (GameSpeed >= trailThreshold && isOnGround && !GameOver);
+
+        if (!trail.isPlaying && enableTrail ) trail.Play();
+        else if (trail.isPlaying && !enableTrail) trail.Stop();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -47,8 +77,11 @@ public class PlayerController : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            Time.timeScale = 1;
+            if (null != explossion) explossion.Play();
             _animator.SetBool(DEATH_ANIMATION, true);
             GameOver = true;
         }
+        
     }
 }
