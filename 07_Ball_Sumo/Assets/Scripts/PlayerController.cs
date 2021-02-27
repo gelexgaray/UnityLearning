c9a2ef255b702537ac441434df1d454ab3d5b2b9
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-02-27 09:37:36 +0100

    Add vertical movement to sumo ball

---
+++ 07_Ball_Sumo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float movementForce = 5;
+    private Rigidbody _rigidbody;
+    public GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody.AddForce(focalPoint.transform.forward * movementForce * Input.GetAxis("Vertical"));
+        
+    }
+}
