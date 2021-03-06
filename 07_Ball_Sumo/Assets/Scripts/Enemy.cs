2ba1f4d4ca58029f5694be103328ac4f6e1e00d4
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-03-06 09:34:32 +0100

    Add first enemy to Ball Sumo

---
+++ 07_Ball_Sumo/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float movementForce = 5;
+    private Rigidbody _rigidbody;
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetDirection = (target.transform.position - this.transform.position).normalized;
+        _rigidbody.AddForce(targetDirection * movementForce);
+
+    }
+
+}
