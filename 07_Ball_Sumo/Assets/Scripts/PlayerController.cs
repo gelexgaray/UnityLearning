ba6bd6a24457559c446cad6867c1d05854628d8a
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-03-13 09:55:26 +0100

    Ball Sumo: Add basic SpawnManager and PowerUps skeleton

--- 07_Ball_Sumo/Assets/Scripts/PlayerController.cs
+++ 07_Ball_Sumo/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float movementForce = 5;
     private Rigidbody _rigidbody;
     public GameObject focalPoint;
+    public bool hasPowerUp;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         _rigidbody.AddForce(focalPoint.transform.forward * movementForce * Input.GetAxis("Vertical"));
-        
+
     }
+
+    /// <summary>
+    /// Get Power UPs
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
+    }
+
 }
---
+++ 07_Ball_Sumo/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private const float spawnRange = 9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = NewRandomSpawnPosition();
+        Instantiate(enemyPrefab, spawnPosition, enemyPrefab.transform.rotation);
+    }
+
+    private static Vector3 NewRandomSpawnPosition()
+    {
+        float posX = Random.Range(-spawnRange, spawnRange);
+        float posZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnPosition = new Vector3(posX, 0, posZ);
+        return spawnPosition;
+    }
+}
