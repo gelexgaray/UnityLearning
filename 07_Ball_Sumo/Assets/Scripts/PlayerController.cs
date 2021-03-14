0f276d41a12e8365049da0659e0f86331c76c91c
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-03-14 09:41:13 +0100

    Ball sumo: Power up implementation with coroutine

--- 07_Ball_Sumo/Assets/Scripts/PlayerController.cs
+++ 07_Ball_Sumo/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rigidbody;
     public GameObject focalPoint;
     public bool hasPowerUp;
+    public float powerUpRepulseForce = 100;
+    public float powerUpSeconds = 7;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +34,26 @@ public class PlayerController : MonoBehaviour
         {
             hasPowerUp = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerUpExpirer());
         }
     }
 
+    /// <summary>
+    /// Apply PowerUps
+    /// </summary>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Enemy") && hasPowerUp)
+        {
+            var enemyRigidBody = collision.collider.gameObject.GetComponent<Rigidbody>();
+            Vector3 repulseDirection = collision.collider.transform.position - this.transform.position;
+            enemyRigidBody.AddForce(repulseDirection * powerUpRepulseForce, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpExpirer() 
+    {
+        yield return new WaitForSeconds(powerUpSeconds);
+        hasPowerUp = false;
+    }
 }
