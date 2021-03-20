dda0912eafece9e3bfb842136664cdbb87e65205
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-03-20 09:39:47 +0100

    PoweUp indicator rings

--- 07_Ball_Sumo/Assets/Scripts/PlayerController.cs
+++ 07_Ball_Sumo/Assets/Scripts/PlayerController.cs
@@ -12,17 +12,22 @@ public class PlayerController : MonoBehaviour
     public float powerUpRepulseForce = 100;
     public float powerUpSeconds = 7;
 
+    private GameObject _powerUpRoot;
+    public GameObject[] _powerUpRings;
+
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        if (_powerUpRings.Length > 0) _powerUpRoot = _powerUpRings[0].transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         _rigidbody.AddForce(focalPoint.transform.forward * movementForce * Input.GetAxis("Vertical"));
-
+        _powerUpRoot.transform.position = this.transform.position;
     }
 
     /// <summary>
@@ -53,7 +58,23 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator PowerUpExpirer() 
     {
-        yield return new WaitForSeconds(powerUpSeconds);
-        hasPowerUp = false;
+        if (_powerUpRings.Length <= 0)
+        {
+            // Timed powerup, without feedback
+            yield return new WaitForSeconds(powerUpSeconds);
+            hasPowerUp = false;
+        }
+        else
+        {
+            // Enable all power up rings, and then disable one by one
+            float timePerRing = powerUpSeconds / _powerUpRings.Length;
+            foreach (GameObject ring in _powerUpRings) ring.SetActive(true);
+            foreach (GameObject ring in _powerUpRings)
+            {
+                yield return new WaitForSeconds(timePerRing);
+                ring.SetActive(false);
+            }
+            hasPowerUp = false;
+        }
     }
 }
