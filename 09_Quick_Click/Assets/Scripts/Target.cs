659d772c2f91943fac863677c453e551c2deffeb
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-04-05 09:51:55 +0200

    Quick Click: object spawn and destroy logic

--- 09_Quick_Click/Assets/Scripts/Target.cs
+++ 09_Quick_Click/Assets/Scripts/Target.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Target : MonoBehaviour
 {
+    private Rigidbody _rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.transform.position = NewRandomStartPosition();
+        _rigidBody = this.GetComponent<Rigidbody>();
+        _rigidBody.AddForce(NewRandomImpulse(), ForceMode.Impulse);
+    }
+
+    private Vector3 NewRandomStartPosition()
+    {
+        const float maxSpawnPosX = 4;
+        const float spawnPosY = -2;
+        var vector3 = new Vector3(Random.Range(-maxSpawnPosX, maxSpawnPosX), spawnPosY);
+        return vector3;
+    }
+
+    private Vector3 NewRandomImpulse()
+    {
+        const float minImpulse = 10;
+        const float maxImpulse = 18;
+        var vector3 = Vector3.up * Random.Range(minImpulse, maxImpulse);
+        return vector3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Destroy when entering sensor at bottom
+        if(other.gameObject.name.Equals("Sensor"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
