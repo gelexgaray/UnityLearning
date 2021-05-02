62fcb570c8a4c4a6f0af48d785514532ebc81518
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-05-02 09:24:49 +0200

    Quick Click: Add difficulty button script

---
+++ 09_Quick_Click/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = this.GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+
+    void OnClick() 
+    {
+        Debug.Log($"'{button.name}' pressed");
+    }
+
+}
