82dd60e770f7b535011d6aa76c084c8d6e1f2217
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-06-06 10:55:30 +0200

    John Lemmon: Basic character movements

---
+++ 11_John_Lemmon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 movement;
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+    private Quaternion rotation = Quaternion.identity;
+
+    [SerializeField, Range(1, 100)]
+    private float turnSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // We sum tho movements of module 1, so the result can be > 1. Normalization is required
+        this.movement.Set(horizontal, 0, vertical);
+        this.movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        _animator.SetBool("IsWalking", isWalking);
+        Vector3 desiredForward = Vector3.RotateTowards(
+            transform.forward,
+            movement,
+            turnSpeed * Time.deltaTime,
+            0f);
+        this.rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        // S = S0 * delta S
+        // Animation is setup to give the movement quantity. We add the movement direction
+        _rigidbody.MovePosition(_rigidbody.position + this.movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(this.rotation);
+    }
+}
